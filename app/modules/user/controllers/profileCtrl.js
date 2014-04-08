39811bf4cc62782b532ede8e7ac4fe84c7ff2b71
fidde <fredrik.christenson@gmail.com>
2014-04-08 18:47:25 +0200

    messed up the commit

---
+++ app/modules/user/controllers/profileCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('user').controller('ProfileCtrl', ['$scope', function($scope){
+    $scope.user = {img: 'modules/user/views/assets/image/user_big.png', name: 'Foo Bar', class: 'Emo', message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, optio, vero, soluta laborum vel nobis provident debitis assumenda necessitatibus dolorum culpa quod laudantium saepe. Laudantium magni amet deleniti repellat dolores?'};
+ 
+  }]);
